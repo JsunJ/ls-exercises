38c9bb4ba1fefef71774dddfec9e412b6f54f939
Jason Jones <JsunJ.Jones@gmail.com>
2021-06-09 21:31:15 -0400

    Update convert-a-string-to-a-signed-number.js

--- js101-small-problems/easy-2/convert-a-string-to-a-signed-number!/convert-a-string-to-a-signed-number.js
+++ js101-small-problems/easy-2/convert-a-string-to-a-signed-number!/convert-a-string-to-a-signed-number.js
@@ -83,3 +83,14 @@ function stringToSignedInteger(str) {
 console.log(stringToSignedInteger("4321") === 4321); // logs true
 console.log(stringToSignedInteger("-570") === -570); // logs true
 console.log(stringToSignedInteger("+100") === 100); // logs true
+
+// function stringToSignedInteger(string) {
+//   switch (string[0]) {
+//     case "-":
+//       return -stringToInteger(string.slice(1, string.length));
+//     case "+":
+//       return stringToInteger(string.slice(1, string.length));
+//     default:
+//       return stringToInteger(string);
+//   }
+// }
