91409499cbde49b8e8f42e268d67252f3411c52c
Jason Jones <JsunJ.Jones@gmail.com>
2021-08-18 18:14:28 -0400

    oo-basics-classes problems 1 through 3

---
+++ js120-object-oriented-javascript/oo-basics-classes/name-the-constructor.js
@@ -0,0 +1,7 @@
+console.log("Hello");
+console.log([1,2,3]);
+console.log({name: 'Srdjan'});
+
+console.log("Hello".constructor.name);
+console.log([1, 2, 3].constructor.name);
+console.log({name: 'Srdjan'}.constructor.name);
