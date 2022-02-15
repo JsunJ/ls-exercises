70e20b179eeb293f684922f26a6aac63a2199db1
Jason Jones <JsunJ.Jones@gmail.com>
2022-02-15 15:28:17 -0500

    added objects problem 3

---
+++ js120-object-oriented-javascript/objects/testing-object-equality.js
@@ -0,0 +1,41 @@
+function objectsEqual(obj1, obj2) {
+  for (let keyOne in obj1) {
+    for (let keyTwo in obj2) {
+      if (keyOne !== keyTwo) return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(objectsEqual({a: 'foo'}, {a: 'foo'}));                      // true
+console.log(objectsEqual({a: 'foo', b: 'bar'}, {a: 'foo'}));            // false
+console.log(objectsEqual({}, {}));                                      // true
+console.log(objectsEqual({a: 'foo', b: undefined}, {a: 'foo', c: 1}));  // false
+
+function objectsEqual(a, b) {
+  if (a === b) {
+    return true;
+  }
+
+  return (keysMatch(a, b) && valuesMatch(a, b));
+}
+
+function keysMatch(a, b) {
+  let aKeys = Object.getOwnPropertyNames(a).sort();
+  let bKeys = Object.getOwnPropertyNames(b).sort();
+
+  if (aKeys.length !== bKeys.length) {
+    return false;
+  }
+
+  return aKeys.every((key, index) => {
+    return key === bKeys[index];
+  });
+}
+
+function valuesMatch(a, b) {
+  let aKeys = Object.getOwnPropertyNames(a).sort();
+
+  return aKeys.every(key => a[key] === b[key]);
+}
