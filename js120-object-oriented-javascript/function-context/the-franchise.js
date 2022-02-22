607183628ff6ca411808e36712c3013a886ca417
Jason Jones <JsunJ.Jones@gmail.com>
2022-02-22 16:11:34 -0500

    added function context problem 2

---
+++ js120-object-oriented-javascript/function-context/the-franchise.js
@@ -0,0 +1,27 @@
+let franchise = {
+  name: 'How to Train Your Dragon',
+  allMovies: function() {
+    return [1, 2, 3].map(number => {
+      return this.name + ' ' + number;
+    });
+  },
+};
+
+/*
+let franchise = {
+  name: 'How to Train Your Dragon',
+  allMovies: function() {
+    let self = this;
+    return [1, 2, 3].map(function(number) {
+      return self.name + ' ' + number;
+    });
+  },
+};
+*/
+
+console.log(franchise.allMovies());
+// [
+//   'How to Train Your Dragon 1',
+//   'How to Train Your Dragon 2',
+//   'How to Train Your Dragon 3'
+// ]
