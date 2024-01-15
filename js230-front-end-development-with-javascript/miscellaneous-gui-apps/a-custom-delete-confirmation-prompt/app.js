6476993cfcd820caa5e390fb0806be7d70235c71
Jason Jones <JsunJ.Jones@gmail.com>
2024-01-15 17:57:19 -0500

    add 'miscellaneous gui apps' exercise 4

--- js230-front-end-development-with-javascript/miscellaneous-gui-apps/a-custom-delete-confirmation-prompt/app.js
+++ js230-front-end-development-with-javascript/miscellaneous-gui-apps/a-custom-delete-confirmation-prompt/app.js
@@ -7,44 +7,61 @@ let todo_items = [
 
 document.addEventListener('DOMContentLoaded', () => {
   const listDiv = document.getElementById('todo_list');
+  const overlayDiv = document.querySelector('.overlay');
+  const promptDiv = document.querySelector('.prompt_container');
+  const contextDiv = document.querySelector('.context_menu');
   const template = document.getElementById('todo_template');
   const todoTmpl = Handlebars.compile(template.innerHTML);
-  const overlayDiv = document.querySelector('.overlay');
-  const prompt = document.querySelector('.prompt_container');
   template.remove();
 
   listDiv.innerHTML = todoTmpl({todos: todo_items});
 
   let todoId;
-  listDiv.addEventListener('click', event => {
-    if (event.target.tagName === 'A') {
-      todoId = event.target.closest('li').dataset.id;
-      let overlayDiv = document.querySelector('.overlay');
 
-      overlayDiv.classList.replace('hide', 'show');
-      prompt.classList.replace('hide', 'show');
+  listDiv.addEventListener('contextmenu', event => {
+    if (event.target.tagName === 'LI') {
+      event.preventDefault();
+      let posX = event.clientX;
+      let posY = event.clientY;
+
+      contextDiv.classList.replace('hide', 'show');
+      contextDiv.style.left = posX + 'px';
+      contextDiv.style.top = posY + 'px';
+
+      todoId = event.target.dataset.id;
     }
   });
 
-  prompt.addEventListener('click', event => {
-    let noBtn = prompt.querySelector('.prompt_no');
-    let yesBtn = prompt.querySelector('.prompt_yes');
+  contextDiv.addEventListener('click', event => {
+    if (event.target.classList.contains('delete')) {
+      overlayDiv.classList.replace('hide', 'show');
+      promptDiv.classList.replace('hide', 'show');
+    }
+  });
+
+  promptDiv.addEventListener('click', event => {
+    let noBtn = promptDiv.querySelector('.prompt_no');
+    let yesBtn = promptDiv.querySelector('.prompt_yes');
 
     if (event.target === noBtn) {
-      prompt.classList.replace('show', 'hide');
+      promptDiv.classList.replace('show', 'hide');
       overlayDiv.classList.replace('show', 'hide');
     } else if (event.target === yesBtn) {
       listDiv.querySelector(`li[data-id='${todoId}'`).remove();
       todo_items = todo_items.filter(todo => {
         return todo.id !== parseInt(todoId, 10);
       });
-      prompt.classList.replace('show', 'hide');
+      promptDiv.classList.replace('show', 'hide');
       overlayDiv.classList.replace('show', 'hide');
     }
   });
 
   overlayDiv.addEventListener('click', event => {
     overlayDiv.classList.replace('show', 'hide');
-    prompt.classList.replace('show', 'hide');
+    promptDiv.classList.replace('show', 'hide');
+  });
+
+  document.body.addEventListener('click', event => {
+    contextDiv.classList.replace('show', 'hide');
   });
 });
